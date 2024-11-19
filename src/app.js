1b2424878799979193dfc9f8f02acebd129712a7
akhil <akhilmengi7@gmail.com>
2024-11-19 23:09:26 +0530

    Dynamic Data signup added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const {connectDB}  = require('./config/database')
 const app = express();
+const User = require('./models/user')
+
+app.use(express.json());
+
+app.post("/signup",async(req,res)=>{
+
+    const user = new User(req.body)
+    try{
+        
+        await user.save();
+        res.send("User added Successfully")
+    }catch(error){
+        res.status(500).send("Error Saving user to  DB"+ error.message)
+    }
+   
+})
+
 
 
 connectDB().then(() => {
